b7bce0d3dba0051d96171e3658d67a740223b0c6
Aninda Kar <andy0104@gmail.com>
2021-12-27 23:55:37 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const title = 'Blog Post';
+  const body = 'This is my blog post!';
+  const comments = [
+    { id: 1, body: 'This is comment 1' },
+    { id: 2, body: 'This is comment 2' },
+    { id: 3, body: 'This is comment 3' }
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>{title}</h1>
+      <p>{body}</p>
+      <div className="comments">
+        <ul>
+          {
+            comments.map((comment, index) => (<li key={index}>{comment.body}</li>))
+          }
+        </ul>
+      </div>
     </div>
   );
 }
